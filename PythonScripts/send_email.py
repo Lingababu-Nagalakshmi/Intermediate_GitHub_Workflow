92bf6679d75d0ea6b1e5749f937bafe62cd285a2
Lingababu Guggilapu <lingababu.guggilapu@perficient.com>
2023-10-06 11:08:36 +0530

    Changes updated

--- PythonScripts/send_email.py
+++ PythonScripts/send_email.py
@@ -8,19 +8,27 @@ smtp_server = "smtp.gmail.com"
 USERNAME = os.environ.get('USER_EMAIL')
 PASSWORD = os.environ.get('USER_PASSWORD')
 
-
 print("Username:", USERNAME)
 print("Password:", PASSWORD)
 
-
-
-message = """
+message = '''
 Subject: GitHub Email report
 This is your daily email Report.
-"""
+'''
 
-# Replace with your actual method to get the password
-context = ssl.create_default_context()
-with smtplib.SMTP_SSL(smtp_server,port,context=context) as server:
-    server.login(USERNAME,PASSWORD)
-    server.sendmail(USERNAME,PASSWORD,message)
+
+if USERNAME is not None and PASSWORD is not None:
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+        try:
+            server.login(USERNAME, PASSWORD)
+            # Send your email here
+        except smtplib.SMTPAuthenticationError as e:
+            print("SMTP Authentication Error:", e)
+        except Exception as e:
+            print("An error occurred:", e)
+else:
+    print("Username or password is not defined or is None.")
+
+
+
