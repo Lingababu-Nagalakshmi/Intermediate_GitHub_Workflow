ca6c7ddc54aace44668d8b7118caf2bec9479bf0
Lingababu Guggilapu <lingababu.guggilapu@perficient.com>
2023-10-06 08:54:21 +0530

    Changes updated

--- PythonScripts/send_email.py
+++ PythonScripts/send_email.py
@@ -8,13 +8,24 @@ smtp_server = "smtp.gmail.com"
 USERNAME = os.environ.get('USER_EMAIL')
 PASSWORD = os.environ.get('USER_PASSWORD')
 
+
+print("Username:", USERNAME)
+print("Password:", PASSWORD)
+
+
+
 message = """
 Subject: GitHub Email report
 This is your daily email Report.
 """
 
-# Encode the password in Base64
-password_base64 = base64.b64encode(PASSWORD.encode()).decode()
+# Replace with your actual method to get the password
+
+if PASSWORD is not None:
+    password_base64 = base64.b64encode(PASSWORD.encode()).decode()
+else:
+    print("Password is not defined or is None.")
+
 
 context = ssl.create_default_context()
 with smtplib.SMTP_SSL(smtp_server,port,context=context) as server:
