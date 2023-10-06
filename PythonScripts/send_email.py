14d5f32ddaf3c1c06a081ca4d809470136621222
Lingababu Guggilapu <lingababu.guggilapu@perficient.com>
2023-10-06 08:43:53 +0530

    Changes updated

--- PythonScripts/send_email.py
+++ PythonScripts/send_email.py
@@ -1,5 +1,7 @@
-import smtblib, ssl
+import smtplib, ssl
 import os
+import base64
+
 
 port = 465
 smtp_server = "smtp.gmail.com"
@@ -11,7 +13,10 @@ Subject: GitHub Email report
 This is your daily email Report.
 """
 
+# Encode the password in Base64
+password_base64 = base64.b64encode(PASSWORD.encode()).decode()
+
 context = ssl.create_default_context()
 with smtplib.SMTP_SSL(smtp_server,port,context=context) as server:
-    server.login(USERNAME,PASSWORD)
-    server.sendmail(USERNAME,PASSWORD,message)
+    server.login(USERNAME,password_base64)
+    server.sendmail(USERNAME,password_base64,message)
