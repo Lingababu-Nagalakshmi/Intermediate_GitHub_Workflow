1433f5a1ec4317ef21a324b1eb87ff7e5d5fca59
Lingababu Guggilapu <lingababu.guggilapu@perficient.com>
2023-10-06 11:22:40 +0530

    Changes updated

--- PythonScripts/send_email.py
+++ PythonScripts/send_email.py
@@ -1,37 +1,19 @@
-import smtplib, ssl
-import os
-import base64
+import smtplib
+import ssl
 
+USERNAME = "guggilapulingababu1@gmail.com"
+PASSWORD = "321@NagaLinga"
 
-port = 465
 smtp_server = "smtp.gmail.com"
-#USERNAME = os.environ.get('USER_EMAIL')
-#PASSWORD = os.environ.get('USER_PASSWORD')
+port = 465  
 
-USERNAME = 'guggilapulingababu1@gmail.com'
-PASSWORD = '321@NagaLinga'
+context = ssl.create_default_context()
 
-print("Username:", USERNAME)
-print("Password:", PASSWORD)
-
-message = '''
-Subject: GitHub Email report
-This is your daily email Report.
-'''
-
-
-if USERNAME is not None and PASSWORD is not None:
-    context = ssl.create_default_context()
+try:
     with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-        try:
-            server.login(USERNAME, PASSWORD)
-            # Send your email here
-        except smtplib.SMTPAuthenticationError as e:
-            print("SMTP Authentication Error:", e)
-        except Exception as e:
-            print("An error occurred:", e)
-else:
-    print("Username or password is not defined or is None.")
-
-
-
+        server.login(USERNAME, PASSWORD)
+        # Send your email here
+except smtplib.SMTPAuthenticationError as e:
+    print("SMTP Authentication Error:", e)
+except Exception as e:
+    print("An error occurred:", e)
