004efa693d642104667b3b970e397385f7b4e0fa
Lingababu Guggilapu <lingababu.guggilapu@perficient.com>
2023-10-06 11:30:52 +0530

    Changes updated

--- PythonScripts/send_email.py
+++ PythonScripts/send_email.py
@@ -2,17 +2,29 @@ import smtplib
 import ssl
 
 USERNAME = "guggilapulingababu1@gmail.com"
+# Use the App Password you generated for your application
 PASSWORD = "321@NagaLinga"
 
-smtp_server = "smtp.gmail.com"
-port = 465  
+print("Username:", USERNAME)
+print("Password:", PASSWORD)
 
+
+
+
+smtp_server = "smtp.gmail.com"
+port = 465  # Google SMTP port
+
+
+message = '''
+Subject: GitHub Email report
+This is your daily email Report.
+'''
 context = ssl.create_default_context()
 
 try:
     with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
         server.login(USERNAME, PASSWORD)
-        # Send your email here
+        server.sendmail(USERNAME,USERNAME,message)
 except smtplib.SMTPAuthenticationError as e:
     print("SMTP Authentication Error:", e)
 except Exception as e:
