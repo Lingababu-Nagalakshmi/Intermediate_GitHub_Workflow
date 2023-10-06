9d2804fa9ca83ee5ef31c11660ed955f4d0cb78d
Lingababu Guggilapu <lingababu.guggilapu@perficient.com>
2023-10-06 12:08:00 +0530

    Changes updated

--- PythonScripts/send_email.py
+++ PythonScripts/send_email.py
@@ -1,21 +1,22 @@
-import smtplib, ssl
+import smtplib
+import base64
 import os
+import ssl
+
 
 smtp_server = "smtp.gmail.com"
 port = 587  
-# sender_email = "guggilapulingababu1@gmail.com"
-# password = "321@NagaLinga"
-
 sender_email = os.environ.get('USER_EMAIL')
 password = os.environ.get('USER_PASSWORD')
 
-
+if password is not None and isinstance(password, str):
+    password_base64 = base64.b64encode(password.encode()).decode()
+else:
+    print("Password is not defined or is not a valid string.")
 
 message = "***************Subject: GitHub Email report****************"
    
-
 context = ssl.create_default_context()
-
 
 try:
     server = smtplib.SMTP(smtp_server,port)
@@ -30,3 +31,8 @@ except Exception as e:
     print(e)
 finally:
     server.quit()
+    
+    
+
+
+
